9af981d5d4e7ea2676c0fccb8439257f3da6a0a4
Leon Franco <leonard.k.franco@gmail.com>
2019-07-02 08:44:23 -0700

    Updated calculator interface

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     // HELPER CLASS //
@@ -23,6 +24,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainTextView.text = "100"
     } // onCreate
 
     private fun pressNumButton(num: Int) {

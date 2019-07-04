04305e1174a2bd80b56cd1431391dae9bb44978b
Leon Franco <leonard.k.franco@gmail.com>
2019-07-04 13:04:20 -0700

    Recreated number button logic and hooked up logic to the number buttons

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.simplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,13 +20,34 @@ class MainActivity : AppCompatActivity() {
     private var modePressed = false
     private val output = StringBuilder()
 
-
     // METHODS //
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mainTextView.text = "HELLO"
+        mainTextView.text = "0"
+
+        val numButtons = arrayOf(button0, button1, button2, button3, button4, button5, button6, button7, button8, button9)
+        initializeNumberButtons(numButtons)
+
+
     } // onCreate
+
+    private fun initializeNumberButtons(numButtons: Array<Button>) {
+        for (button in numButtons) {
+            button.setOnClickListener {
+                val tempString = StringBuilder(mainTextView.text)
+
+                when (tempString.toString()) {
+                    "0" -> tempString[0] = button.text[0]
+                    else -> tempString.append(button.text)
+                }
+
+                mainTextView.text = tempString.toString()
+                output.append(button.text)
+                modePressed = false
+            } // setOnClickListener
+        } // for
+    } // initializeNumberButtons
 
 } // MainActivity
